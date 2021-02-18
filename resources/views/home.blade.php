c40bde48e8c1641709924310f23b7d452aaca7be
viewtifult <tashanduncan1994@gmail.com>
2021-02-18 16:19:27 +0000

    Added authentication

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,23 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>iSquat - Workout Logger</title>
-    <link rel="stylesheet" href="{{asset('/css/bootstrap.min.css')}}">
+@extends('layouts.app')
 
-</head>
-<body>
-<div class="container text-center">
-    <img src="{{asset('/images/isquatlogo.png')}}">
-    <h1>Workout Logger</h1>
-    <button  class="btn btn-primary">Workouts</button>
-    <button  class="btn btn-primary">Programs</button>
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
-
-</body>
-</html>
+@endsection
