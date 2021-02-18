989ca4deec0844b73df500aed75eb64572c9fc37
viewtifult <tashanduncan1994@gmail.com>
2021-02-18 15:29:13 +0000

    Add Home Page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>iSquat - Workout Logger</title>
+    <link rel="stylesheet" href="{{asset('/css/bootstrap.min.css')}}">
+
+</head>
+<body>
+<div class="container text-center">
+    <img src="{{asset('/images/isquatlogo.png')}}">
+    <h1>Workout Logger</h1>
+    <button  class="btn btn-primary">Workouts</button>
+    <button  class="btn btn-primary">Programs</button>
+</div>
+
+</body>
+</html>
